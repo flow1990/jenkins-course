37e62894c4028b53a432e900cd0dae34768493bf
flow1990 <90790070+flow1990@users.noreply.github.com>
2023-01-13 18:12:58 +0100

    Credentials

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git(https://github.com/flow1990/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('f.walliser@gmx.de')
         }
     }
     triggers {
